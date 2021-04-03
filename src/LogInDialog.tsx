b875bc3247f9f5c2bb94869766f9048517f52992
Bobby Kalaf <bobby.kalaf@junk-in-the-trunk.com>
2021-04-03 11:46:56 -0700

    chore(cleanup): cleanup of older files

--- src/LogInDialog.tsx
+++ src/LogInDialog.tsx
@@ -1,5 +1,18 @@
 export function LogInDialog() {
-    return <div className="w-full h-auto max-w-md">
+    return <div className="w-full h-auto max-w-md ">
         <div>Login Dialog</div>
     </div>
 }
+
+# Modules
++ Authentication
++ Authorization
++ Logging
++ Data Validation
++ Domain
++ Error Handling
++ Internationalization and Localization
++ Persistence
++ Memory
++ Monitoring
+
